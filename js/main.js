d0f2655950e74073db5b54bcbd3823ad8e4dfd19
Gelena Dmitrieva <dmitrievaGelenaOneTwo@gmail.com>
2020-12-28 02:13:20 +0300

    ad adaptive

--- js/main.js
+++ js/main.js
@@ -3,9 +3,9 @@ $(function() {
         slidesToShow: 1,
         slidesToScroll: 1,
         arrows: true,
-        prevArrow: '<button class="slick-arrow prev-arrow"><img src="images/slider/prev.svg" alt=""></button>',
-        nextArrow: '<button class="slick-arrow next-arrow"><img src="images/slider/next.svg" alt=""></button>',
         fade: true,
+        prevArrow: '<div class="prev-arrow"><img src="images/slider/prev.svg" alt=""></div>',
+        nextArrow: '<div class="next-arrow"><img src="images/slider/next.svg" alt=""></div>',
         asNavFor: '.slider__nav'
     });
     $('.slider__nav').slick({
@@ -14,9 +14,42 @@ $(function() {
         asNavFor: '.slider__for',
         dots: false,
         arrows: false,
-        focusOnSelect: true
+        focusOnSelect: true,
+        responsive: [{
+                breakpoint: 940,
+                settings: {
+                    slidesToShow: 5,
+                    slidesToScroll: 1
+                }
+            },
+            {
+                breakpoint: 770,
+                settings: {
+                    slidesToShow: 4,
+                    slidesToScroll: 1
+                }
+            },
+            {
+                breakpoint: 620,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 1
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1
+                }
+            },
+        ]
+    });
+    $('.menu__btn').click(function() {
+        $('.header__menu ul').slideToggle();
     });
 
-
-
 });
+
+// prevArrow: '<span type="button" class="slick-arrow prev-arrow"><img src="images/slider/prev.svg" alt=""></span>',
+// nextArrow: '<span type="button" class="slick-arrow next-arrow"><img src="images/slider/next.svg" alt=""></span>',
